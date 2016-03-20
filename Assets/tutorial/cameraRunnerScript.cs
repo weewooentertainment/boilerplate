15fe54dbccf64ad0e7dbb8e09a171b0a342af0df
Dennis Yau <denn.yau@gmail.com>
2016-03-20 16:14:56 -0400

    saved scene

---
+++ Assets/tutorial/cameraRunnerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraRunnerScript : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x + 6, 5, -15);
+	}
+}
---
+++ Assets/tutorial/destroyerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyerScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			Debug.Break ();
+		}
+		// blow up parent if it's there
+		if (other.gameObject.transform.parent) {
+			Destroy (other.gameObject.transform.parent.gameObject);
+		} else {
+			Destroy (other.gameObject);
+		}
+	}
+}
